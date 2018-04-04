915c43edf201852bd827ab5c377a3e106c26e74a
oTongXuanAn <tong.xuan.an@framgia.com>
2018-04-04 17:25:54 +0700

    test

--- app/src/main/java/com/example/framgiatongxuanan/viblo_shimmer_effect/adapter/AndroidVersionAdapter.kt
+++ app/src/main/java/com/example/framgiatongxuanan/viblo_shimmer_effect/adapter/AndroidVersionAdapter.kt
@@ -12,7 +12,7 @@ import kotlinx.android.synthetic.main.item_android_version.view.*
 /**
  * Created by FRAMGIA\tong.xuan.an on 08/01/2018.
  */
-    class AndroidVersionAdapter(private val androidVersionList: List<AndroidVersion>) : RecyclerView.Adapter<AndroidVersionAdapter.ViewHolder>() {
+class AndroidVersionAdapter(private val androidVersionList: List<AndroidVersion>) : RecyclerView.Adapter<AndroidVersionAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_android_version, parent, false)
         return ViewHolder(view)
@@ -25,7 +25,6 @@ import kotlinx.android.synthetic.main.item_android_version.view.*
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         fun bind(android: AndroidVersion) {
-            //code nothing
             itemView.tv_name.text = android.name
             itemView.tv_version.text = android.ver
             itemView.tv_api_level.text = android.api
